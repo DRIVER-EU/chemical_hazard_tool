f28f4e3b05ca8e4638517ae8297c7a2ea423e594
Erik Vullings <erik.vullings@gmail.com>
2020-05-28 17:54:02 +0200

    Include proj4leaflet in order to use EPSG:28992 for OpenTopo

--- packages/gui/src/app.ts
+++ packages/gui/src/app.ts
@@ -1,6 +1,7 @@
 import 'material-icons/iconfont/material-icons.css';
 import 'materialize-css/dist/css/materialize.min.css';
 import m from 'mithril';
+import 'proj4leaflet';
 import './css/style.css';
 import { dashboardSvc } from './services/dashboard-service';
 
